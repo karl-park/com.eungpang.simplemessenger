e9e0abbe56a6364357cc24b499539e7d750eba76
KarlPark <karl.panki.park@gmail.com>
2021-10-15 21:14:28 +0800

    Add a feature to add a new chatting bot

--- app/src/main/java/com/eungpang/simplemessenger/data/repository/ChatRepository.kt
+++ app/src/main/java/com/eungpang/simplemessenger/data/repository/ChatRepository.kt
@@ -1,12 +1,8 @@
 package com.eungpang.simplemessenger.data.repository
 
-import android.app.Application
-import android.content.Context
 import com.eungpang.simplemessenger.data.database.SimpleMessengerDatabase
 import com.eungpang.simplemessenger.data.entity.MessageEntity
 import com.eungpang.simplemessenger.domain.chat.Message
-import com.eungpang.simplemessenger.shared.ConstantPref
-import java.util.*
 
 interface ChatRepository {
     suspend fun retrieveChatHistory(
@@ -29,91 +25,6 @@ class ChatRepositoryImpl(
 
     override suspend fun sendMessage(message: Message): Boolean {
         database.messageDao().saveMessage(MessageEntity.mapFrom(message))
-        return true
-    }
-}
-
-class MockChatRepositoryImpl(
-    private val applicationContext: Context
-): ChatRepository {
-    private val userId: String
-    init {
-        val pref = applicationContext.getSharedPreferences(ConstantPref.KEY_PREF_NAME, Application.MODE_PRIVATE)
-        userId = pref.getString(ConstantPref.KEY_USER_ID, "")!!
-    }
-
-    private val chatRepository = mutableMapOf<String, MutableList<Message>>().apply {
-        val bot1 = "${userId}-echobot-1"
-        val bot2 = "${userId}-echobot-2"
-        val bot1Room = listOf(userId, bot1).sorted().joinToString("||")
-        val bot2Room = listOf(userId, bot2).sorted().joinToString("||")
-
-        this[bot1Room] = mutableListOf(
-            Message(
-                userId,
-                bot1Room,
-                "Hi, Nice to meet you.",
-                Date(1634225659469L),
-                null,
-            ),
-            Message(
-                userId,
-                bot1Room,
-                "Excuse me, can you hear me?",
-                Date(1634239659469L),
-                null,
-            ),
-            Message(
-                userId,
-                bot1Room,
-                "Have a nice day!",
-                Date(1634249659469L),
-                null,
-            ),
-            Message(
-                "${userId}-echobot-1",
-                bot1Room,
-                "Good to see you.",
-                Date(1634249659469L),
-                null,
-            )
-        )
-
-        this[bot2Room] = mutableListOf(
-            Message(
-                userId,
-                bot2Room,
-                "Hi, Kent! How are you?",
-                Date(1634225659469L),
-                null,
-            ),
-            Message(
-                userId,
-                bot2Room,
-                "It's been a while.",
-                Date(1634249659469L),
-                null,
-            ),
-            Message(
-                "${userId}-echobot-2",
-                bot2Room,
-                "Have a nice day man!",
-                Date(1634249659469L),
-                null,
-            )
-        )
-    }
-
-    override suspend fun retrieveChatHistory(
-        roomId: String,
-        page: Int,
-        limit: Int
-    ): List<Message> {
-        return chatRepository[roomId] ?: listOf()
-    }
-
-    override suspend fun sendMessage(message: Message): Boolean {
-        chatRepository[message.roomId]?.add(message)
         return true
     }
 }
--- app/src/main/java/com/eungpang/simplemessenger/ui/friends/viewmodel/FriendsViewModel.kt
+++ app/src/main/java/com/eungpang/simplemessenger/ui/friends/viewmodel/FriendsViewModel.kt
@@ -8,20 +8,22 @@ import androidx.lifecycle.viewModelScope
 import com.eungpang.simplemessenger.domain.chat.GetChatHistoryUseCase
 import com.eungpang.simplemessenger.domain.chat.Message
 import com.eungpang.simplemessenger.domain.common.Result
+import com.eungpang.simplemessenger.domain.friend.AddUserProfileUseCase
 import com.eungpang.simplemessenger.domain.friend.GetFriendsListUseCase
 import com.eungpang.simplemessenger.domain.friend.Profile
 import com.eungpang.simplemessenger.shared.ConstantPref
 import com.eungpang.simplemessenger.shared.livedata.SingleLiveEvent
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
-import timber.log.Timber
+import java.util.*
 import javax.inject.Inject
 
 @HiltViewModel
 class FriendsViewModel @Inject constructor(
     app: Application,
     private val getFriendsListUseCase: GetFriendsListUseCase,
-    private val getChatHistoryUseCase: GetChatHistoryUseCase
+    private val getChatHistoryUseCase: GetChatHistoryUseCase,
+    private val addUserProfileUseCase: AddUserProfileUseCase,
 ) : AndroidViewModel(app) {
     private val userId: String
 
@@ -84,9 +86,37 @@ class FriendsViewModel @Inject constructor(
         }
     }
 
+    fun isValidName(name: String) : Boolean {
+        if (name.length > 10) return false
+
+        return true
+    }
+
+    fun addNewBot(name: String) {
+        viewModelScope.launch {
+            val botProfile = Profile(
+                "${userId}-${name}",
+                name,
+                BOT_AVATAR_URL_LIST.random(),
+                null,
+                null,
+                Date()
+            )
+            when (val result = addUserProfileUseCase.invoke(botProfile)) {
+                is Result.Success -> {
+                    val friendList = _friends.value ?: emptyList()
+                    _friends.postValue(friendList.toMutableList().apply {
+                        add(FriendViewItem(botProfile, null, ::onClickFriendViewItem))
+                    })
+                }
+                else -> {
+                    actionState.postValue(ActionState.ShowToastMessage("Sorry, try again."))
+                }
+            }
+        }
+    }
+
     private fun onClickFriendViewItem(profile: Profile) {
-        // Todo: need to redirect user to Conversation View
-        Timber.d("Friend View Item Clicked: $profile")
         actionState.postValue(ActionState.GoToConversationView(profile))
     }
 
@@ -94,6 +124,26 @@ class FriendsViewModel @Inject constructor(
         // TODO: do refactoring to pass just loggedInId and friendId
         //  RoomId should be calculated from UseCases or Repositories.
         return listOf(loggedInId, friendId).sorted().joinToString("||")
+    }
+
+    companion object {
+        val BOT_AVATAR_URL_LIST = listOf(
+            "https://www.jenkins.io/images/logos/JCasC/JCasC.png",
+            "https://www.jenkins.io/images/logos/actor/actor.png",
+            "https://www.jenkins.io/images/logos/alien/alien.png",
+            "https://www.jenkins.io/images/logos/amsterdam/amsterdam.png",
+            "https://www.jenkins.io/images/logos/atomium/atomium.png",
+            "https://www.jenkins.io/images/logos/austin/austin.png",
+            "https://www.jenkins.io/images/logos/automotive/automotive.png",
+            "https://www.jenkins.io/images/logos/balloon/balloon.png",
+            "https://www.jenkins.io/images/logos/barcelona/barcelona.png",
+            "https://www.jenkins.io/images/logos/boston/boston.png",
+            "https://www.jenkins.io/images/logos/brno/brno.png",
+            "https://www.jenkins.io/images/logos/cambridge/cambridge.png",
+            "https://www.jenkins.io/images/logos/clown/clown.png",
+            "https://www.jenkins.io/images/logos/cute/cute.png",
+            "https://www.jenkins.io/images/logos/evergreen/evergreen.png",
+        )
     }
 }
 
